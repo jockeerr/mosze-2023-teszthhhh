aa0d24f6e1d9c01e4aae3f01e2d261354d71a4a3
jockeerr <62694962+jockeerr@users.noreply.github.com>
2023-09-23 17:11:26 +0200

    Update main.cpp
    yooooooooo

--- main.cpp
+++ main.cpp
@@ -4,21 +4,21 @@ constexpr int N_ELEMENTS = 100;
 
 int main()
 {
-    int *b = new int[NELEMENTS];                       //N_ELEMENTS
-    std::cout << '1-100 ertekek duplazasa'             //"" nem '' a cout után, endl és ; a végére 
+    int *b = new int[N_ELEMENTS];                       //N_ELEMENTS
+    std::cout << "1-100 ertekek duplazasa"<<endl;             //"" nem '' a cout után, endl és ; a végére 
     for (int i = 0;)                                   //for ciklus kevés feltétellel
     {                                                        
         b[i] = i * 2;                                    
     }                                                    
     for (int i = 0; i; i++)                                    
     {                                                    
-        std::cout << "Ertek:"                          //nincs ;
+        std::cout << "Ertek:";                          //nincs ;
     }                                                  
     std::cout << "Atlag szamitasa: " << std::endl;     
     int atlag;                                         
-    for (int i = 0; i < N_ELEMENTS, i++)               //; N_ELEMENTS után
+    for (int i = 0; i < N_ELEMENTS; i++)               //; N_ELEMENTS után
     {                                                  
-        atlag += b[i]                                  //nincs ;
+        atlag += b[i];                                  //nincs ;
     }
     atlag /= N_ELEMENTS;
     std::cout << "Atlag: " << atlag << std::endl;
